72da0ef6d828e3a98909d14a1705e60027ec8870
insooth <insooth@users.noreply.github.com>
2019-01-19 22:46:06 +0100

    Create 4f6dfd77685f73b1.cpp

---
+++ coliru/4f6dfd77685f73b1.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+
+using A = int;
+using B = int;
+
+struct S { A a; B b; };
+
+// uncomment some code
+
+int main()
+{
+    B b;
+    S s;
+    
+//    (s.b = b).a;
+//    decltype(s.b = b) x = "foo";
+//    (s.b).xx;
+    
+    return 0;
+}
